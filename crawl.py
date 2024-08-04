a872e4bd390f2f6906c08eaf126732646e3da0f0
David <102869@isp.cz>
2024-08-04 13:40:51 +0200

    Initial commit

---
+++ crawl.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import torrent
+
+url = 'https://pcgamestorrents.com/games-list-pc-616134483-torrent-download-free.html'  # Replace with the actual URL
+
+# Send a GET request to the site
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Find all <a> elements within <li> that have rel="noopener noreferrer"
+    links = soup.select('li > a[rel="noopener noreferrer"]')
+    
+    link_data = []
+    session = requests.Session()
+
+    for i, link in enumerate(links[:5]):
+        #start = time.time()
+        #print(f"{i + 1}/{len(links)} | ETA: {(length * (len(links) - 1))/60.0:.2f}m")
+        
+        magnet, size, date = torrent.get(session, link.get('href'))
+        
+        link_info = {
+            'title': link.get_text(strip=True),
+            'uris': [magnet],
+            'uploadDate': date,
+            'fileSize': size
+        }
+
+        link_data.append(link_info)
+    
+    export = {
+        'name': 'IGGGames',
+        'downloads': link_data
+    }
+    
+    # Convert the list of dictionaries to JSON with actual symbols
+    json_data = json.dumps(export, ensure_ascii=False, indent=4)
+    
+    # Write the JSON data to a file with UTF-8 encoding
+    with open('links.json', 'w', encoding='utf-8') as json_file:
+        json_file.write(json_data)
+    
+    print("Data has been written to links.json")
+else:
+    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
---
+++ torrent.py
@@ -0,0 +1,55 @@
+import requests
+import re
+
+
+def bluemediafiles_decode_key(encoded):
+    key = ''
+    half_len = len(encoded) // 2
+    for i in range(half_len - 5, -1, -2):
+        key += encoded[i]
+    for i in range(half_len + 4, len(encoded), 2):
+        key += encoded[i]
+    return key
+
+# Compile regex patterns once
+game_button_pattern = re.compile(r'<a\s+href="(https://gamedownloadurl\.lol/[^"]+)"')
+encoded_key_pattern = re.compile(r'Goroi_n_Create_Button\("(?P<encoded>[^"]+)"\);')
+magnet_link_pattern = re.compile(r'<a class="button" href="(magnet:[^"]+)"')
+file_size_pattern = re.compile(r'<span class="uk-text-meta">Release Size: </span>(\d+(?:\.\d+)?(?:\s?(?:MB|GB)))</p>')
+datetime_pattern = re.compile(r'<time datetime="([^"]+)"')
+
+def get(session, url):
+    
+    try:
+        response = session.get(url)
+        response.raise_for_status()
+
+        game_button = game_button_pattern.search(response.text)
+        file_size = file_size_pattern.search(response.text)
+        datetime = datetime_pattern.search(response.text)
+
+        if game_button:
+            game_url = game_button.group(1)
+            response = session.get(game_url)
+            response.raise_for_status()
+
+        # Search for the encoded key in the response text
+            encoded_key = None
+        match = encoded_key_pattern.search(response.text)
+        if match:
+            encoded_key = match.group('encoded')
+
+        if encoded_key:
+            decoded_key = bluemediafiles_decode_key(encoded_key)
+            redirect_url = f'https://gamedownloadurl.lol/get-url.php?url={decoded_key}'
+            response = session.get(redirect_url)
+            response.raise_for_status()
+            magnet_link = magnet_link_pattern.search(response.text)
+            if magnet_link:
+                return magnet_link.group(1), file_size.group(1), datetime.group(1)
+                
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+    return None
